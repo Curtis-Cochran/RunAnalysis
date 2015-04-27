702cc1dc89e3d83e3b6429d757f66ee958aef966
Curtis-Cochran <jccochran@gmail.com>
2015-04-26 22:57:37 -0400

    eight commit

--- downloadZip.R
+++ downloadZip.R
@@ -1,3 +1,5 @@
+#This script downloads and unzips the UCI HAR data set to your working directory
+
 fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 download.file(fileUrl,destfile="c:/DataScienceFiles/DataScience_Project/samsungdata.zip")
 unzip("c:/DataScienceFiles/DataScience_Project/samsungdata.zip",overwrite=TRUE,unzip="internal")
